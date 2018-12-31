21112a52f925d01ee1efef17f20ee7dad3cf8fea
dmullen17 <dmullen17@gmail.com>
2018-12-31 14:26:18 -0800

    finished project

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-let sayHi = function() {
+function sayHi() {
     console.log('Hi');
 }
 
-let toggleOpen = function() {
+function toggleOpen() {
     //console.log(this);
     this.classList.toggle('open');
 }
 
-let addOpenActive = function(event) {
+function addOpenActive(event) {
     /*let classes = Array.from(this.classList);
     console.log(classes);
     if (classes.includes('open')) {
         this.classList.add('open-active');
     }*/
     console.log(event);
+    console.log(event.propertyName); //font-size flex-grow and transform are all transitioning. - we will specify only flex
     if (event.propertyName.includes('flex')) {
         this.classList.toggle('open-active');
     }
@@ -23,5 +24,5 @@ let panels = document.querySelectorAll('.panel');
 //panels.forEach(panel => panel.addEventListener('click', sayHi));
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend', addOpenActive))
+panels.forEach(panel => panel.addEventListener('transitionend', addOpenActive));
                
